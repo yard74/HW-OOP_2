51b381af2f2044f532c4e7f552c0f040aed6fb55
Alexandr Antyaskin <forest-home74@mail.ru>
2021-10-20 23:28:26 +0500

    Task #4 done

--- main.py
+++ main.py
@@ -150,3 +150,88 @@ class Reviewer(Mentor):
     def __str__(self):
         reviewer_info = f'Имя: {self.name}\nФамилия: {self.surname}'
         return reviewer_info
+
+
+best_student = Student('Петя', 'Васечкин', 'male')
+best_student.courses_in_progress += ['Python']
+best_student.courses_in_progress += ['GIT']
+best_student.finished_courses += ['English for programmers']
+
+worst_student = Student('Ваня', 'Двоечкин', 'male')
+worst_student.courses_in_progress += ['Python']
+worst_student.courses_in_progress += ['GIT']
+
+student_list = [best_student, worst_student]
+
+cool_lecturer = Lecturer('Анна', 'Васильева')
+cool_lecturer.courses_attached += ['Python']
+cool_lecturer.courses_attached += ['GIT']
+
+worst_lecturer = Lecturer('Наталья', 'Кукушкина')
+worst_lecturer.courses_attached += ['Python']
+worst_lecturer.courses_attached += ['GIT']
+
+lecturer_list = [cool_lecturer, worst_lecturer]
+
+cool_reviewer = Reviewer('Иван', 'Иванов')
+cool_reviewer.courses_attached += ['Python']
+cool_reviewer.courses_attached += ['GIT']
+
+best_reviewer = Reviewer('Илья', 'Соболев')
+best_reviewer.courses_attached += ['Python']
+best_reviewer.courses_attached += ['GIT']
+
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 8)
+cool_reviewer.rate_hw(best_student, 'GIT', 10)
+
+best_reviewer.rate_hw(worst_student, 'Python', 6)
+best_reviewer.rate_hw(worst_student, 'GIT', 2)
+best_reviewer.rate_hw(worst_student, 'GIT', 4)
+
+best_student.rate_lecture(cool_lecturer, 'Python', 10)
+best_student.rate_lecture(cool_lecturer, 'Python', 8)
+best_student.rate_lecture(cool_lecturer, 'GIT', 9)
+best_student.rate_lecture(cool_lecturer, 'GIT', 8)
+best_student.rate_lecture(cool_lecturer, 'GIT', 9)
+
+worst_student.rate_lecture(worst_lecturer, 'Python', 10)
+worst_student.rate_lecture(worst_lecturer, 'Python', 8)
+worst_student.rate_lecture(worst_lecturer, 'GIT', 9)
+worst_student.rate_lecture(worst_lecturer, 'GIT', 7)
+worst_student.rate_lecture(worst_lecturer, 'GIT', 5)
+
+
+def get_avg_hw_grade_in_course(stud_list, course):
+    sum_grades = 0
+    count_grades = 0
+    for student in stud_list:
+        sum_grades += sum(student.grades[course])
+        count_grades += len(student.grades[course])
+    return print(round((sum_grades / count_grades), 2))
+
+
+def get_avg_lecture_grade_in_course(lec_list, course):
+    sum_grades = 0
+    count_grades = 0
+    for lecturer in lec_list:
+        sum_grades += sum(lecturer.grades[course])
+        count_grades += len(lecturer.grades[course])
+    return print(round((sum_grades / count_grades), 2))
+
+
+print(f'Проверка принта Ревьюэра:\n{cool_reviewer}')
+print()
+print(f'Проверка принта Лектора:\n{cool_lecturer}')
+print()
+print(f'Проверка принта Студента:\n{best_student}')
+print()
+print(f'Проверка сравнения студентов:\n{best_student <= worst_student}')
+print()
+print(f'Проверка сравнения лекторов:\n{cool_lecturer > worst_lecturer}')
+print()
+print('Проверка работы функции расчета средней оценки за ДЗ всех студентов в рамках курса GIT:')
+get_avg_hw_grade_in_course(student_list, 'GIT')
+print()
+print('Проверка работы функции расчета средней оценки за лекции всех лекторов в рамках курса GIT:')
+get_avg_hw_grade_in_course(lecturer_list, 'GIT')
